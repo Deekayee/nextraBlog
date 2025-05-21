27be18ff2cca9339c391a390f64918378c77a095
Deekayee <nesterenko91@gmail.com>
2025-05-21 05:52:22 +0100

    cool logo thing

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,12 +12,36 @@ export const metadata = {
 
 export default async function RootLayout({ children }) {
   return (
-    <html lang='en' suppressHydrationWarning>
-      <Head backgroundColor={{ dark: '#0f172a', light: '#fefce8' }} />
+    <html lang='pt-PT' suppressHydrationWarning>
+      <Head backgroundColor={{ dark: '#0f172a', light: '#fefce8' }}>
+        <link rel='preconnect' href='https://fonts.googleapis.com'></link>
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        ></link>
+        <link
+          href='https://fonts.googleapis.com/css2?family=Vampiro+One&display=swap'
+          rel='stylesheet'
+        ></link>
+      </Head>
       <body>
         <Layout>
           <Navbar pageMap={await getPageMap()}>
-            <Search placeholder='Procurar no blog' />
+            <a
+              href='/'
+              style={{
+                textDecoration: 'none',
+                fontWeight: 800,
+                marginRight: 'auto',
+                fontSize: '2rem',
+                fontFamily: 'Vampiro One',
+                marginBottom: 0,
+              }}
+            >
+              Deekayee
+            </a>
+            <Search placeholder='Procurar...' />
             <ThemeSwitch />
           </Navbar>
 
@@ -25,7 +49,13 @@ export default async function RootLayout({ children }) {
 
           <Footer>
             {new Date().getFullYear()} © Made with ❤️ and Nextra by{' '}
-            <a href='https://github.com/Deekayee'>Deekayee</a>.
+            <a
+              style={{ fontFamily: 'Vampiro One' }}
+              href='https://github.com/Deekayee'
+            >
+              Deekayee
+            </a>
+            .
             <a href='/feed.xml' style={{ float: 'right' }}>
               RSS
             </a>
